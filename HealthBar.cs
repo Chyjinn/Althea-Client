c6ca3cdbddbaa414a7c490eca8de6f69adb56ffe
Bendeguz Torok <morcsog99@gmail.com>
2023-06-16 17:53:52 +0200

    healthbar

---
+++ HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using RAGE;
+using RAGE.Elements;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Client
+{
+    internal class HealthBar: Events.Script
+    {
+        RAGE.Ui.HtmlWindow CEF;
+        public HealthBar()
+        {
+            Events.Tick += UpdateHealth;
+            CEF = new RAGE.Ui.HtmlWindow("package:\\health\test.html");
+            //"C:\RAGEMP\server-files\client_packages\health\test.html"
+            CEF.Active = true;
+        }
+
+        private void UpdateHealth(List<Events.TickNametagData> nametags)
+        {
+            CEF.ExecuteJs($"Update(\"{Player.LocalPlayer.GetHealth()}\")");
+
+        }
+
+    }
+}
