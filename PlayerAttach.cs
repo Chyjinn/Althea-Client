c80550910fc2b3a7cd2385e30841b85ce4caba54
morcsog <morcsog99@gmail.com>
2023-06-17 15:36:15 +0200

    Box creation client side
    Kliens oldalon létrehoz egy dobozt és törli is disconnectnél.

--- PlayerAttach.cs
+++ PlayerAttach.cs
@@ -1,18 +1,17 @@
 ﻿using RAGE;
-using RAGE.Game;
-using RAGE;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Drawing;
 
 namespace Client
 {
     internal class PlayerAttach : Events.Script
     {
+        private List<int> objList = new List<int>();
         public PlayerAttach()
         {
             Events.Add("client:getGroundHeight", getGroundHeight);
+            Events.Add("client:createBox", createBox);
+            Events.OnPlayerQuit += playerQuit;
         }
 
         public void getGroundHeight(object[] args)
@@ -25,6 +24,28 @@ namespace Client
             lastArgsToVectorZ = argsToVector.Z;
             //Chat.Output("Lefut a kliens" + lastArgsToVectorZ);
             Events.CallRemote("server:getGroundHeight", lastArgsToVectorZ);
+        }
+
+        public void createBox(object[] args)
+        {
+            var player = (RAGE.Elements.Player)args[0];
+            var obj = RAGE.Game.Object.CreateObject(2930714276, player.Position.X, player.Position.Y, player.Position.Z, false, false, false);
+            //RAGE.Chat.Output("Var obj kiíratás: " + obj);
+            objList.Add(obj);
+
+
+        }
+
+        public void playerQuit(RAGE.Elements.Player client)
+        {            
+            if (client.RemoteId == RAGE.Elements.Player.LocalPlayer.RemoteId)
+            {
+                foreach (var item in objList)
+                {
+                    int refInt = item;
+                    RAGE.Game.Object.DeleteObject(ref refInt);
+                }
+            }
         }
 
     }
