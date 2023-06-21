151d02fd80b334bff1ecd1ff2600e107ce5eedf8
DanielMate Cziczka <DanielMate.Cziczka@harman.com>
2023-06-21 09:40:15 +0200

    kommentelés

--- Auth/LoginRegister.cs
+++ Auth/LoginRegister.cs
@@ -28,7 +28,7 @@ namespace Client.Login
         TokenData t;
         public LoginRegister()
         {
-            Events.OnPlayerReady += GetTokenFromJS;
+            Events.OnPlayerReady += GetTokenFromJS;//amint betöltött a játék megpróbáljuk betölteni a login tokent
 
             Events.Add("client:ShowLoginForm", ShowLoginForm);
             Events.Add("client:ShowRegisterForm", ShowRegisterForm);
@@ -44,6 +44,44 @@ namespace Client.Login
             Events.Add("client:LoadToken", LoadToken);
         }
 
+        private void GetTokenFromJS()//indításnál fut le (onPlayerReady) -> client:LoadToken-t hívja majd vissza
+        {
+            Events.CallLocal("js:loadToken");
+        }
+
+        private void SaveToken(object[] args)//szerver meghívja, mentjük .storage-ba a tokent
+        {
+            uint accid = Convert.ToUInt32(args[0]);
+            string token = args[1].ToString();
+            string expiration = args[2].ToString();
+            Events.CallLocal("js:saveToken", accid, token, expiration);
+        }
+
+
+        public void LoginAttempt(object[] args)
+        {
+            Events.CallRemote("server:LoginAttempt", (string)args[0], (string)args[1], (bool)args[2]);
+        }
+
+        public void RegisterAttempt(object[] args)
+        {
+            Events.CallRemote("server:RegisterAttempt", (string)args[0], (string)args[1], (string)args[2], (string)args[3]);
+        }
+        private void LoginAttemptWithToken(object[] args)
+        {
+            Events.CallRemote("server:LoginAttemptWithToken", t.accID, t.tokenString);
+        }
+
+
+        public void SetHudState(bool flag)
+        {
+            RAGE.Nametags.Enabled = flag;
+            RAGE.Game.Ui.DisplayRadar(flag);
+            RAGE.Chat.Show(flag);
+        }
+
+
+
         private void IncorrectToken(object[] args)
         {
             AuthCEF.Active = false;
@@ -54,26 +92,20 @@ namespace Client.Login
             SaveToken(arg);
         }
 
-        private void GetTokenFromJS()
-        {
-            Events.CallLocal("js:loadToken");
-        }
 
-        private void LoginAttemptWithToken(object[] args)
-        {
-            Events.CallRemote("server:LoginAttemptWithToken", t.accID, t.tokenString);
-        }
 
-        private void LoadToken(object[] args)
+        private void LoadToken(object[] args)//A "js:loadToken" ezt fogja meghívni, itt vizsgálni kell hogy valid token van-e ha a player belenyúlna (jó adatokat kapunk-e, lejárt-e a token)
         {
-            if (args[0].ToString() == "0" || args[1].ToString() == "tokenerror" || args[2].ToString() == "0")
+            //TODO: kezelni ha a user belenyúl a tokenbe
+            if (args[0].ToString() == "0" || args[1].ToString() == "tokenerror" || args[2].ToString() == "0")//ha nem tudja beolvasni a JS a tokent akkor is ezeket az értékeket kapjuk
             {
                 ProcessLoginScreen(false);
             }
             else
             {
+                //jó adatokat kaptunk
                 t = new TokenData(Convert.ToUInt32(args[0]), args[1].ToString(), Convert.ToDateTime(args[2]));
-                if (t.expiration > DateTime.Now)
+                if (t.expiration > DateTime.Now)//megvizsgáljuk hogy lejárt-e már a helyi token, alapból egyezik az adatbázissal de ha a user átírja akkor lehet megkapja ezt, szerver oldalon is kell ellenőrizni
                 {
                     ProcessLoginScreen(true);
                 }
@@ -81,30 +113,16 @@ namespace Client.Login
 
         }
 
-        private void SaveToken(object[] args)
+        public void ProcessLoginScreen(bool type)//type=true -> token-es login | type=false -> sima login
         {
-            uint accid = Convert.ToUInt32(args[0]);
-            string token = args[1].ToString();
-            string expiration = args[2].ToString();
-            Chat.Output("Új token" + expiration);
-            Events.CallLocal("js:saveToken", accid,token,expiration);
-        }
-
-        public void ProcessLoginScreen(bool type)
-        {
-            for (int i = 0; i < 362; i++)
-            {
-                Pad.DisableControlAction(0, i, true);
-            }
-
             SetHudState(true);
-            
             RAGE.Elements.Player.LocalPlayer.FreezePosition(false);
-            if (type)
+
+            if (type)//tokenlogin
             {
                 AuthCEF = new RAGE.Ui.HtmlWindow("package://frontend/auth/tokenlogin.html");
             }
-            else
+            else//sima login
             {
                 AuthCEF = new RAGE.Ui.HtmlWindow("package://frontend/auth/login.html");
             }
@@ -124,36 +142,21 @@ namespace Client.Login
             AuthCEF.Destroy();
         }
 
-        public void ShowLoginForm(object[] args)
+        public void ShowLoginForm(object[] args)//CEF hívja
         {
             DestroyAuthForm();
             AuthCEF = new RAGE.Ui.HtmlWindow("package://frontend/auth/login.html");
             AuthCEF.Active = true;
         }
 
-        public void ShowRegisterForm(object[] args)
+        public void ShowRegisterForm(object[] args)//CEF hívja
         {
             DestroyAuthForm();
             AuthCEF = new RAGE.Ui.HtmlWindow("package://frontend/auth/register.html");
             AuthCEF.Active = true;
         }
 
-        public void LoginAttempt(object[] args)
-        {
-            Events.CallRemote("server:LoginAttempt", (string)args[0], (string)args[1], (bool)args[2]);
-        }
 
-        public void RegisterAttempt(object[] args)
-        {
-            Events.CallRemote("server:RegisterAttempt", (string)args[0], (string)args[1], (string)args[2], (string)args[3]);
-        }
-
-        public void SetHudState(bool flag)
-        {
-            RAGE.Nametags.Enabled = flag;
-            RAGE.Game.Ui.DisplayRadar(flag);
-            RAGE.Chat.Show(flag);
-        }
 
 
 
