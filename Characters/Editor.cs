56bd1b9a7d77d9deeec6ea1ec5d6afd7c62e7489
Chyjinn <chyjinn@gmail.com>
2023-08-22 21:11:27 +0200

    Update Editor.cs

--- Characters/Editor.cs
+++ Characters/Editor.cs
@@ -14,9 +14,36 @@ namespace Client.Characters
 
         public void CharacterEditor(object[] args)
         {
+            Events.Tick += Tick;
+            RAGE.Input.TakeScreenshot("valami.png", 1, 100, 100);
             //CAM -813.95, 174.2, 76.78, 0, 0, -69
             //kamera megvan, karakter is a helyén
             //CEF-et megnyitni
+        }
+        private void Tick(List<Events.TickNametagData> nametags)
+        {
+            if (RAGE.Input.IsDown(RAGE.Ui.VirtualKeys.Right))
+            {
+                RotateCharRight();
+            }
+            else if (RAGE.Input.IsDown(RAGE.Ui.VirtualKeys.Left))
+            {
+                RotateCharLeft();
+            }
+            
+        }
+
+        
+
+    private void RotateCharRight()
+        {
+            Events.CallRemote("server:RotateCharRight");
+        }
+
+        private void RotateCharLeft()
+        {
+
+            Events.CallRemote("server:RotateCharLeft");
         }
     }
 }
