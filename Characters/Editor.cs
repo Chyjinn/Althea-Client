09e365b8f286b422b4230ba55ddb843cbaee6f7e
DanielMate Cziczka <DanielMate.Cziczka@harman.com>
2023-08-31 11:38:31 +0200

    Editor kamera kezelés

--- Characters/Editor.cs
+++ Characters/Editor.cs
@@ -1,4 +1,5 @@
 ﻿using RAGE;
+using RAGE.Elements;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -26,18 +27,52 @@ namespace Client.Characters
         {
             Events.CallRemote("server:EditAttribute", Convert.ToInt32(args[0]), Convert.ToString(args[1]));//attribute id, value
         }
+        int camera = -1;
+        public void SetCamera()
+        {
+            Vector3 pos = Player.LocalPlayer.Position;
 
+            float radians = -Player.LocalPlayer.GetHeading() * (float)Math.PI / 180f;
+            float nx = pos.X + (1.2f * (float)Math.Sin(radians));
+            float ny = pos.Y + (1.2f * (float)Math.Cos(radians));
 
-    
+            camera = RAGE.Game.Cam.CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", nx, ny, pos.Z+0.7f, 0f, 0f, 0f, 60f, true, 2);
+            RAGE.Game.Cam.PointCamAtCoord(camera, pos.X, pos.Y, pos.Z);
+            RAGE.Game.Cam.SetCamActive(camera, true);
+            RAGE.Game.Cam.RenderScriptCams(true, true, 500, true, false, 0);
+        }
+
+        public void DeleteCamera()
+        {
+            RAGE.Game.Cam.DestroyCam(camera, true);
+            RAGE.Game.Cam.SetCamActive(camera, false);
+            RAGE.Game.Cam.RenderScriptCams(false, true, 500, true, false, 0);
+        }
+
 
         public void CharacterEditor(object[] args)
         {
-            EditorCEF = new RAGE.Ui.HtmlWindow("package://frontend/editor/charedit.html");
-            EditorCEF.Active = true;
+            bool state = (bool)args[0];
 
-            Events.Tick += Tick;
+            if (state)
+            {
+                EditorCEF = new RAGE.Ui.HtmlWindow("package://frontend/editor/charedit.html");
+                EditorCEF.Active = true;
+                SetCamera();
+                Events.Tick += Tick;
+            }
+            else
+            {
+                EditorCEF.Active = false;
+                EditorCEF.Destroy();
+                DeleteCamera();
+                Events.Tick -= Tick;
+            }
+
 
         }
+
+
         private void Tick(List<Events.TickNametagData> nametags)
         {
             if (RAGE.Input.IsDown(RAGE.Ui.VirtualKeys.Right))
@@ -48,7 +83,6 @@ namespace Client.Characters
             {
                 RotateCharLeft();
             }
-            
         }
 
         
