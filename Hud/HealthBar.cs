ce534221a0b0f575c898751a1ccab8586e619847
Chyjinn <chyjinn@gmail.com>
2023-09-02 13:57:02 +0200

    Update HealthBar.cs

--- Hud/HealthBar.cs
+++ Hud/HealthBar.cs
@@ -12,11 +12,16 @@ namespace Client.Hud
             //CEF = new RAGE.Ui.HtmlWindow("package://hp/test.html");
             //"C:\RAGEMP\server-files\client_packages\health\test.html"
             //CEF.Active = true;
-            //Events.Tick += UpdateHealth;
+            Events.Tick += UpdateHealth;
         }
 
         private void UpdateHealth(List<Events.TickNametagData> nametags)
         {
+            RAGE.Game.Ui.HideHudComponentThisFrame(6);
+            RAGE.Game.Ui.HideHudComponentThisFrame(7);
+            RAGE.Game.Ui.HideHudComponentThisFrame(8);
+            RAGE.Game.Ui.HideHudComponentThisFrame(9);
+            RAGE.Game.Ui.HideHudComponentThisFrame(20);
             //CEF.ExecuteJs($"Update(\"{RAGE.Elements.Player.LocalPlayer.GetHealth()}\")");
         }
 
