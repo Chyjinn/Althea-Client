3f7b4b110eb2b5fb50a93720031aab6922315380
Chyjinn <chyjinn@gmail.com>
2023-11-06 16:31:19 +0100

    Update NameTag.cs

--- Hud/NameTag.cs
+++ Hud/NameTag.cs
@@ -1,4 +1,5 @@
-﻿using RAGE;
+﻿using Client.Inventory;
+using RAGE;
 using RAGE.Elements;
 using RAGE.Ui;
 using System;
@@ -29,7 +30,7 @@ namespace Client.Hud
         const float height = 0.0065f;
         const float border = 0.001f;
 
-        
+        static HtmlWindow NametagCEF;
         HtmlWindow ChatCEF;
         HtmlWindow VersionCEF;
 
@@ -40,11 +41,14 @@ namespace Client.Hud
             
             Events.Add("client:Chat", ShowChat);
             Events.Add("client:BindKeys", BindKeys);
-
+            Events.Add("client:NametagTest", NametagTest);
 
             RAGE.Chat.Show(false);
             ChatCEF = new HtmlWindow("package://frontend/chat/index.html");
             ChatCEF.Active = false;
+
+            NametagCEF = new HtmlWindow("package://frontend/nametag/nametag.html");
+            NametagCEF.Active = true;
 
             VersionCEF = new HtmlWindow("package://frontend/version/ver.html");
             VersionCEF.Active = true;
@@ -85,7 +89,7 @@ namespace Client.Hud
         {
             if (status)
             {
-                Events.Tick += Render;
+               
                 RAGE.Game.Graphics.RequestStreamedTextureDict("3dtextures", true);
             }
             else
@@ -94,53 +98,55 @@ namespace Client.Hud
             }
         }
 
+
+        static List<Ped> peds = new List<Ped>();
+        private static void NametagTest(object[] args)
+        {
+            for (int i = -50; i < +50; i++)
+            {
+                Ped p = new Ped(RAGE.Game.Misc.GetHashKey("mp_m_freemode_01"), new Vector3(RAGE.Elements.Player.LocalPlayer.Position.X + i, RAGE.Elements.Player.LocalPlayer.Position.Y + i, RAGE.Elements.Player.LocalPlayer.Position.Z), 0f, 0);
+                peds.Add(p);
+            }
+            Events.Tick += Render;
+
+
+        }
+
         private static void Render(List<Events.TickNametagData> nametags)
         {
-            
-            if (nametags != null)
+            NametagCEF.ExecuteJs($"clearNametags()");
+            foreach (var item in peds)
             {
-                foreach (var item in nametags)
+                int screenX = 2560;
+                int screenY = 1440;
+                float x = 0;
+                float y = 0;
+
+                RAGE.Game.Graphics.GetScreenCoordFromWorldCoord(item.Position.X, item.Position.Y, item.Position.Z, ref x, ref y);
+                //Chat.Output(item.Id+" X:" + x + " Y:" + y);
+                if (x > -1f && x < 1f && y > -1f && y < 1f)
                 {
-                    if (item.Distance <= maxDistance)
-                    {
-                        RAGE.Elements.Player p = item.Player;
-                        
-                        Color nametagcolor = Color.White;
-                        float scale = (maxDistance / item.Distance) / 5;
-                        int screenX = 1920;
-                        int screenY = 1080;
-                        float x = item.ScreenX;
-                        float y = item.ScreenY;
-
-
-                            bool state = Convert.ToBoolean(p.GetSharedData("player:AdminDuty"));
-                            if (state)
-                            {
-                                nametagcolor = Color.Orange;
-                            if (RAGE.Game.Graphics.HasStreamedTextureDictLoaded("3dtextures"))
-                            {
-                                Chat.Output("be van töltve he");
-                            }
-                                RAGE.Game.Graphics.DrawSprite("helicopterhud", "hudarrow", screenX * x, screenY * y, 0.1f, 0.1f, p.GetHeading(), 255, 0, 0, 255, 50);
-                            }
-
-                        if (scale > 0.6f)
-                        {
-                            scale = 0.6f;
-                        }
-                        y -= scale * (0.005f * (screenY / 1080)); 
-                        
-                        
-                        string name = Convert.ToString(p.GetSharedData("player:VisibleName"));
-                        name = name.Replace('_', ' ');
-                        int id = p.RemoteId;
-                        RAGE.NUI.UIResText.Draw(name+" ("+id+")", Convert.ToInt32(screenX * x),Convert.ToInt32(screenY * y),  RAGE.Game.Font.ChaletComprimeCologne, scale, nametagcolor, RAGE.NUI.UIResText.Alignment.Centered, true, true, 300);
-                        RAGE.Game.Graphics.GetActiveScreenResolution(ref screenX, ref screenY);
-                        RAGE.Game.Graphics.DrawSprite("3dtextures", "mpgroundlogo_bikers", screenX * x, screenY * y, 0.1f, 0.1f, p.GetHeading(), 255, 0, 255, 120, 255);
-
-                    }
+                    NametagCEF.ExecuteJs($"renderNametag(\"{item.Id}\",\"{item.Id}\",\"{Convert.ToInt32(screenX * x)}\",\"{Convert.ToInt32(screenY * y)}\",\"{0.6f}\")");
                 }
+
+                    //string name = Convert.ToString(p.GetSharedData("player:VisibleName"));
+                    //name = name.Replace('_', ' ');
+                    //int id = p.RemoteId;
+                    //RAGE.NUI.UIResText.Draw(name + " (" + id + ")", Convert.ToInt32(screenX * x), Convert.ToInt32(screenY * y), RAGE.Game.Font.ChaletComprimeCologne, scale, nametagcolor, RAGE.NUI.UIResText.Alignment.Centered, true, true, 300);
+                    //RAGE.Game.Graphics.GetActiveScreenResolution(ref screenX, ref screenY);
+                    //RAGE.Game.Graphics.DrawSprite("3dtextures", "mpgroundlogo_bikers", screenX * x, screenY * y, 0.1f, 0.1f, p.GetHeading(), 255, 0, 255, 120, 255);
+
+                    //Chat.Output("X:" + Convert.ToInt32(screenX * x) + "Y:" + Convert.ToInt32(screenY * y) + "SCALE:" + scale);
+                   
+                    //NametagCEF.ExecuteJs($"renderNametag(\"{0}\",\"{name}\",\"{Convert.ToInt32(screenX * x)}\",\"{Convert.ToInt32(screenY * y)}\",\"{scale}\")");
+
+                
+
             }
+
+            
+            // InventoryCEF.ExecuteJs($"addItemToSlot(\"{item.DBID}\",\"{item.ItemID}\",\"{GetItemNameById(item.ItemID)}\",\"{GetItemDescriptionById(item.ItemID)}\",\"{GetItemWeightById(item.ItemID)}\",\"{item.ItemAmount}\",\"{GetItemPicture(item.ItemID)}\",\"{item.Priority}\")");
+
         }
     }
 }
